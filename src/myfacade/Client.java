f0fc2eeaae55fe9db48e77212762c405e0ae04f4
student <student@IBLAB732P-00.ib.polsl.pl>
2018-04-16 12:13:42 +0200

    v2.0

--- src/myfacade/Client.java
+++ src/myfacade/Client.java
@@ -26,17 +26,16 @@ public class Client {
 
         sys1.doSomething3();
         sys3.doSomething1();
-        sys1.doSomething2();
+        sys2.doSomething2();
 
         sys3.doSomething1();
         sys3.doSomething2();
 
-        sys3.doSomething1();
-        sys1.doSomething2();
-
         // or
+        
         doSomethingClient1(sys1, sys2, sys3);
         doSomethingClient2(sys3);
+        doSomethingClient3(sys1, sys2, sys3);
 
         /* Fasada  */
         //...
@@ -45,11 +44,18 @@ public class Client {
     public static void doSomethingClient1(SystemClass1 sys1, SystemClass2 sys2, SystemClass3 sys3) {
         sys1.doSomething3();
         sys3.doSomething1();
-        sys1.doSomething2();
+        sys2.doSomething2();
     }
 
     public static void doSomethingClient2(SystemClass3 sys3) {
         sys3.doSomething1();
         sys3.doSomething2();
     }
+    
+     public static void doSomethingClient3(SystemClass1 sys1, SystemClass2 sys2, SystemClass3 sys3) {
+        sys1.doSomething1();
+        sys3.doSomething2();
+        sys2.doSomething2();
+    }
+
 }
