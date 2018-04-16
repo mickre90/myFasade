291e963ccadd5b3d6fa5b6c15aac4cd1836f387d
DominatorMK <DominatorMK@DESKTOP-B0JP85S>
2018-04-16 09:52:17 +0200

    v1.0

---
+++ src/MySystem/SystemClass2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MySystem;
+
+/**
+ *
+ * @author DominatorMK
+ */
+public class SystemClass2 {
+
+    public void doSomething1() {
+    }
+
+    public void doSomething2() {
+    }
+
+    public void doSomething3() {
+    }
+}
---
+++ src/myfacade/Client.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfacade;
+
+import MySystem.SystemClass1;
+import MySystem.SystemClass2;
+import MySystem.SystemClass3;
+
+/**
+ *
+ * @author LeopardProMK
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Anti-pattern  */
+        SystemClass1 sys1 = new SystemClass1();
+        SystemClass2 sys2 = new SystemClass2();
+        SystemClass3 sys3 = new SystemClass3();
+
+        sys1.doSomething3();
+        sys3.doSomething1();
+        sys1.doSomething2();
+
+        sys3.doSomething1();
+        sys3.doSomething2();
+
+        sys3.doSomething1();
+        sys1.doSomething2();
+
+        // or
+        doSomethingClient1(sys1, sys2, sys3);
+        doSomethingClient2(sys3);
+
+        /* Fasada  */
+        //...
+    }
+
+    public static void doSomethingClient1(SystemClass1 sys1, SystemClass2 sys2, SystemClass3 sys3) {
+        sys1.doSomething3();
+        sys3.doSomething1();
+        sys1.doSomething2();
+    }
+
+    public static void doSomethingClient2(SystemClass3 sys3) {
+        sys3.doSomething1();
+        sys3.doSomething2();
+    }
+}
